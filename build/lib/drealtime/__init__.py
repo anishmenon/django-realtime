e7223aeb60c27a5a571e6c7f9d7703d280826b70
Anish Menon <anish@inzane.in>
2014-11-06 13:56:54 +0530

    Update __init__.py
    Use simplejson . 
    
    pip install simplejson 
    
     'cse of newer version of django depricated simplejson

--- build/lib/drealtime/__init__.py
+++ build/lib/drealtime/__init__.py
@@ -1,7 +1,11 @@
 import httplib
 import socket
 from django.conf import settings
-from django.utils import simplejson as json
+
+try:
+    from django.utils import simplejson as json
+except:
+    import simplejson as json
 
 __version__ = '0.1.5'
 VERSION = map(int, __version__.split('.'))
